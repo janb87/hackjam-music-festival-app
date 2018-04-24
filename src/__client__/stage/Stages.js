eb9eb3c94a7fe47917870e4f3875fe2523beeeeb
Jan Bevers <jan.bevers@kunstmaan.be>
2018-04-24 20:11:29 +0200

    Display different stages

--- src/__client__/stage/Stages.js
+++ src/__client__/stage/Stages.js
@@ -1,18 +1,46 @@
-import React, {Component} from 'react';
-import {ScrollView, View} from 'react-native';
+import React, { Component } from 'react';
+import { ScrollView, View } from 'react-native';
 import axios from 'axios';
-import {serverUrl} from '../config/server';
+import { serverUrl } from '../config/server';
 import StageOverviewCard from './StageOverviewCard';
 
-
-// TODO
-// get ALL stages and display them (Use the StageOverViewCard component to display one Stage)
-// Get all the stages: axios.get(`${serverUrl}/stages`);
-
 class Stages extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stages: null,
+      errorMessage: null
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const request = await axios.get(`${serverUrl}/stages`);
+      this.setState({
+        stages: request.data
+      });
+    } catch (error) {
+      this.setState({
+        errorMessage: error.message
+      });
+    }
+  }
 
   render() {
-    return null;
+    const { stages, errorMessage } = this.state;
+
+    if (!stages) {
+      return null;
+    }
+
+    if (errorMessage) {
+      return <Text>{errorMessage}</Text>;
+    }
+
+    return stages.map(stage => (
+      <StageOverviewCard key={stage.id} stage={stage} />
+    ));
   }
 }
 
